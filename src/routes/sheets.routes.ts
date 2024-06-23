4e8a0bb591195bf9a96947e75b4addbc034f1afd
Travis-Berthrong <33703020+Travis-Berthrong@users.noreply.github.com>
2024-06-23 19:31:00 +0200

    Feat: Table data vvalidation now converts non-numerical values to zero instead of removing them

--- src/routes/sheets.routes.ts
+++ src/routes/sheets.routes.ts
@@ -18,7 +18,7 @@ const parseCsvData = async (csvFile: Express.Multer.File): Promise<string[][] | 
     fs.createReadStream(filePath)
       .pipe(fastCsv.parse({ headers: true }))
       .on('data', (row) => {
-        row = [...Object.values(row)].filter((value: string) => !isNaN(parseFloat(value)))
+        row = [...Object.values(row)].map((value: string) => !isNaN(parseFloat(value))? value : 0)
         console.log('Row:', row);
         results.push(row);
       })
